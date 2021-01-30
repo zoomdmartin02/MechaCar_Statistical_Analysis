1458b09134d5e98e91b04a0d22a256aaed407dd0
zoomdmartin02 <72090048+zoomdmartin02@users.noreply.github.com>
2021-01-29 20:02:42 -0600

    Deliv 1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,5 @@
+library(dplyr)
+library(tidyselect)
+MechaCar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_table) #generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_table))
